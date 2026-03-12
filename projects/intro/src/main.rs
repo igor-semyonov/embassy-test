0066ccaea2d358493ba56e76a35ef3d29b75fabd
Igor Semyonov <igor@semyonov.xyz>
2026-03-12 15:59:11 -0400

    changed futures to be generic

--- projects/intro/src/main.rs
+++ projects/intro/src/main.rs
@@ -6,11 +6,23 @@ use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_futures::join::join;
 use embassy_nrf::{
-    Peri,
-    gpio::{AnyPin, Input, Pull},
+    Peri, PeripheralType,
+    gpio::{AnyPin, Input, Pin, Pull},
+};
+use embassy_sync::{
+    blocking_mutex::raw::CriticalSectionRawMutex,
+    signal::Signal,
 };
 use embassy_time::Timer;
 use panic_probe as _;
+
+enum Button {
+    A,
+    B,
+}
+
+static SIGNAL: Signal<CriticalSectionRawMutex, Button> =
+    Signal::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -18,20 +30,21 @@ async fn main(spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
 
     let button_a = button_task(
-        p.P0_14
-            .into(),
+        p.P0_14,
         "A",
     );
     let button_b = button_task(
-        p.P0_23
-            .into(),
+        p.P0_23,
         "B",
     );
-    join(button_a, button_b).await;
+    join(
+        button_a, button_b,
+    )
+    .await;
 }
 
-async fn button_task(
-    button_pin: Peri<'static, AnyPin>,
+async fn button_task<P: Pin>(
+    button_pin: Peri<'static, P>,
     id: &'static str,
 ) {
     info!(
