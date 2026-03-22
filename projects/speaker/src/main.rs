5437ecdc8401431c7794de9eba8387d8a2c6502d
Igor Semyonov <igor@semyonov.xyz>
2026-03-22 00:16:27 -0400

    A sequence of noises that is almost, but not quite entirely, unlike L'amour Toujours

--- projects/speaker/src/main.rs
+++ projects/speaker/src/main.rs
@@ -20,27 +20,106 @@ async fn main(spawner: Spawner) {
             make_tone(
                 p.P0_00
                     .into(),
-                1_000 / 110,
             ),
         )
         .unwrap();
 }
 
 #[embassy_executor::task]
-async fn make_tone(
-    speaker_pin: Peri<'static, AnyPin>,
-    period: u64,
-) {
+async fn make_tone(speaker_pin: Peri<'static, AnyPin>) {
+    async fn play_tone(
+        speaker: &mut Output<'_>,
+        frequency: u64,
+        cycles: u64,
+    ) {
+        let period = 1_000 / frequency;
+        for _ in 0..cycles / period {
+            speaker.set_high();
+            Timer::after_millis(period / 2).await;
+            speaker.set_low();
+            Timer::after_millis(period / 2).await;
+        }
+    }
     let mut speaker = Output::new(
         speaker_pin,
         Level::Low,
         OutputDrive::Standard,
     );
 
-    for _ in 0..2_000 / period {
-        speaker.set_high();
-        Timer::after_millis(period / 2).await;
-        speaker.set_low();
-        Timer::after_millis(period / 2).await;
+    for held_note in [NOTE_B3, NOTE_A3, NOTE_G3, NOTE_D3] {
+        play_tone(
+            &mut speaker,
+            NOTE_FS4,
+            TONE_FULL,
+        )
+        .await;
+        Timer::after_millis(MS_BETWEEN_TONES);
+
+        play_tone(
+            &mut speaker,
+            NOTE_FS4,
+            TONE_FULL,
+        )
+        .await;
+        Timer::after_millis(MS_BETWEEN_TONES);
+
+        play_tone(
+            &mut speaker,
+            NOTE_FS4,
+            TONE_HALF,
+        )
+        .await;
+        Timer::after_millis(MS_BETWEEN_TONES);
+
+        play_tone(
+            &mut speaker,
+            NOTE_G4,
+            TONE_HALF,
+        )
+        .await;
+        Timer::after_millis(MS_BETWEEN_TONES);
+
+        play_tone(
+            &mut speaker,
+            NOTE_FS4,
+            TONE_FULL,
+        )
+        .await;
+        Timer::after_millis(MS_BETWEEN_TONES);
+
+        play_tone(
+            &mut speaker,
+            NOTE_D4,
+            TONE_FULL,
+        )
+        .await;
+        Timer::after_millis(MS_BETWEEN_TONES);
+
+        play_tone(
+            &mut speaker,
+            held_note,
+            TONE_FULL * 2,
+        )
+        .await;
+
+        Timer::after_millis(MS_BETWEEN_PHRASES);
     }
 }
+
+const MS_BETWEEN_TONES: u64 = 150;
+const MS_BETWEEN_PHRASES: u64 = 400;
+
+const BPM: u64 = 160;
+const MSPB: u64 = 60_000 / BPM;
+const TONE_FULL: u64 = MSPB;
+const TONE_HALF: u64 = MSPB / 2;
+const TONE_QUARTER: u64 = MSPB / 4;
+const TONE_EIGHTH: u64 = MSPB / 8;
+
+const NOTE_FS4: u64 = 370;
+const NOTE_G4: u64 = 392;
+const NOTE_D4: u64 = 294;
+const NOTE_B3: u64 = 247;
+const NOTE_A3: u64 = 220;
+const NOTE_G3: u64 = 196;
+const NOTE_D3: u64 = 147;
