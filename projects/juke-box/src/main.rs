3d63cddd4aef73348d45cb6091fcfc588feafa17
Igor Semyonov <igor@semyonov.xyz>
2026-03-24 16:04:02 -0400

    jukebox working with multiple songs

--- projects/juke-box/src/main.rs
+++ projects/juke-box/src/main.rs
@@ -1,14 +1,12 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+// use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_nrf::{
-    Peri,
-    gpio::{
-        AnyPin, Input, Level, Output, OutputDrive, Pull,
-    },
+use embassy_futures::select::{Either, select};
+use embassy_nrf::gpio::{
+    Input, Level, Output, OutputDrive, Pull,
 };
 use embassy_time::Timer;
 use panic_halt as _;
@@ -24,12 +22,12 @@ const MICROS_PER_BEAT: u64 = 60_000_000 / BEATS_PER_MINUTE;
 // const TONE_EIGHTH: u64 = MICROS_PER_BEAT / 8;
 
 struct Note {
-    frequency: NoteFrequency,
+    frequency: Option<NoteFrequency>,
     duration: NoteDuration,
 }
 impl Note {
     const fn new(
-        frequency: NoteFrequency,
+        frequency: Option<NoteFrequency>,
         duration: NoteDuration,
     ) -> Self {
         Self {
@@ -38,55 +36,55 @@ impl Note {
         }
     }
     async fn play(&self, speaker: &mut Output<'_>) {
-        let period = self.period_micros();
-        for _ in 0..self
-            .duration
-            .micros()
-            / period
-        {
-            speaker.set_high();
-            Timer::after_micros(period / 2).await;
-            speaker.set_low();
-            Timer::after_micros(period / 2).await;
+        if let Some(frequency) = &self.frequency {
+            let period = frequency.period_micros();
+            for _ in 0..self
+                .duration
+                .micros()
+                / period
+            {
+                speaker.set_high();
+                Timer::after_micros(period / 2).await;
+                speaker.set_low();
+                Timer::after_micros(period / 2).await;
+            }
+        } else {
+            Timer::after_micros(
+                self.duration
+                    .micros(),
+            )
+            .await;
         }
-    }
-    const fn frequency(&self) -> u64 {
-        self.frequency
-            .frequency()
-    }
-    const fn period_millis(&self) -> u64 {
-        1_000
-            / self
-                .frequency
-                .frequency()
-    }
-    const fn period_micros(&self) -> u64 {
-        1_000_000
-            / self
-                .frequency
-                .frequency()
     }
 }
 enum NoteFrequency {
-    Fs4,
     G4,
+    Fs4,
+    E4,
     D4,
     B3,
     A3,
     G3,
     D3,
+    B2,
+    G2,
+    Fs2,
 }
 impl NoteFrequency {
     const fn frequency(&self) -> u64 {
         use NoteFrequency as N;
         match self {
-            N::Fs4 => 370,
             N::G4 => 392,
+            N::Fs4 => 370,
+            N::E4 => 330,
             N::D4 => 294,
             N::B3 => 247,
             N::A3 => 220,
             N::G3 => 196,
             N::D3 => 147,
+            N::B2 => 123,
+            N::G2 => 98,
+            N::Fs2 => 93,
         }
     }
     const fn period_millis(&self) -> u64 {
@@ -121,13 +119,13 @@ impl NoteDuration {
     }
 }
 
-struct Phrase<const N_Notes: usize> {
-    notes: [Note; N_Notes],
+struct Phrase {
+    notes: &'static [Note],
     pause: u64,
 }
-impl<const N_Notes: usize> Phrase<N_Notes> {
+impl Phrase {
     const fn new(
-        notes: [Note; N_Notes],
+        notes: &'static [Note],
         pause: u64,
     ) -> Self {
         Self {
@@ -136,7 +134,7 @@ impl<const N_Notes: usize> Phrase<N_Notes> {
         }
     }
     async fn play(&self, speaker: &mut Output<'_>) {
-        for note in &self.notes {
+        for note in self.notes {
             note.play(speaker)
                 .await;
             Timer::after_millis(self.pause).await;
@@ -144,20 +142,13 @@ impl<const N_Notes: usize> Phrase<N_Notes> {
     }
 }
 
-struct Song<
-    const N_Phrases: usize,
-    const N_Notes_per_Phrase: usize,
-> {
-    phrases: [Phrase<N_Notes_per_Phrase>; N_Phrases],
+struct Song {
+    phrases: &'static [Phrase],
     pause: u64,
 }
-impl<
-    const N_Phrases: usize,
-    const N_Notes_per_Phrase: usize,
-> Song<N_Phrases, N_Notes_per_Phrase>
-{
+impl Song {
     const fn new(
-        phrases: [Phrase<N_Notes_per_Phrase>; N_Phrases],
+        phrases: &'static [Phrase],
         pause: u64,
     ) -> Self {
         Self {
@@ -166,7 +157,7 @@ impl<
         }
     }
     async fn play(&self, speaker: &mut Output<'_>) {
-        for phrase in &self.phrases {
+        for phrase in self.phrases {
             phrase
                 .play(speaker)
                 .await;
@@ -175,137 +166,245 @@ impl<
     }
 }
 
+static mut CURRENT_SONG_IDX: usize = 0;
+static SONGS: &[&Song] = &[
+    &LAMOUR,
+    &REMEMBER_VOCAL,
+    &REMEMBER_BASSLINE,
+];
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+
+    let mut button_a = Input::new(
+        p.P0_14,
+        Pull::Up,
+    );
+    let mut button_b = Input::new(
+        p.P0_23,
+        Pull::Up,
+    );
+    let mut speaker = Output::new(
+        p.P0_00,
+        Level::Low,
+        OutputDrive::Standard,
+    );
+
+    loop {
+        let race_result = select(
+            play_noise(
+                &mut speaker,
+                &mut button_a,
+            ),
+            button_b.wait_for_falling_edge(),
+        )
+        .await;
+        match race_result {
+            Either::First(_) => {}
+            Either::Second(_) => unsafe {
+                CURRENT_SONG_IDX += 1;
+                if CURRENT_SONG_IDX  == SONGS.len(){
+                    CURRENT_SONG_IDX = 0;
+                }
+            },
+        };
+    }
+}
+
+async fn play_noise(
+    speaker: &mut Output<'_>,
+    button: &mut Input<'_>,
+) {
+    loop {
+        // reading from CURRENT_SONG_IDX should be safe in this context since the tasks aer
+        // executing cooperatively
+        unsafe {
+            SONGS[CURRENT_SONG_IDX]
+                .play(speaker)
+                .await;
+        }
+        button
+            .wait_for_falling_edge()
+            .await;
+    }
+}
+
 use NoteDuration as ND;
 use NoteFrequency as NF;
-static LAMOUR: Song<4, 7> = Song::new(
-    [
+static REMEMBER_VOCAL: Song = Song::new(
+    &[Phrase::new(
+        &[
+            Note::new(
+                Some(NF::Fs4),
+                ND::Eighth,
+            ),
+            Note::new(
+                Some(NF::E4),
+                ND::Eighth,
+            ),
+            Note::new(
+                Some(NF::D4),
+                ND::Eighth,
+            ),
+            Note::new(
+                Some(NF::B3),
+                ND::Full,
+            ),
+            Note::new(
+                None, // Rest
+                ND::Full,
+            ),
+            Note::new(
+                Some(NF::Fs4),
+                ND::Eighth,
+            ),
+            Note::new(
+                Some(NF::E4),
+                ND::Eighth,
+            ),
+            Note::new(
+                Some(NF::D4),
+                ND::Eighth,
+            ),
+            Note::new(
+                Some(NF::B3),
+                ND::Full,
+            ),
+        ],
+        25,
+    )],
+    400,
+);
+static LAMOUR: Song = Song::new(
+    &[
         Phrase::new(
-            [
+            &[
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::G4,
+                    Some(NF::G4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::D4,
+                    Some(NF::D4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::B3,
+                    Some(NF::B3),
                     ND::Full,
                 ),
             ],
             25,
         ),
         Phrase::new(
-            [
+            &[
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::G4,
+                    Some(NF::G4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::D4,
+                    Some(NF::D4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::A3,
+                    Some(NF::A3),
                     ND::Full,
                 ),
             ],
             25,
         ),
         Phrase::new(
-            [
+            &[
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::G4,
+                    Some(NF::G4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::D4,
+                    Some(NF::D4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::G3,
+                    Some(NF::G3),
                     ND::Full,
                 ),
             ],
             25,
         ),
         Phrase::new(
-            [
+            &[
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::Fs4,
+                    Some(NF::Fs4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::G4,
+                    Some(NF::G4),
                     ND::Half,
                 ),
                 Note::new(
-                    NF::D4,
+                    Some(NF::D4),
                     ND::Full,
                 ),
                 Note::new(
-                    NF::D3,
+                    Some(NF::D3),
                     ND::Full,
                 ),
             ],
@@ -314,105 +413,43 @@ static LAMOUR: Song<4, 7> = Song::new(
     ],
     400,
 );
-
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    let p = embassy_nrf::init(Default::default());
-
-    spawner
-        .spawn(
-            play_noise(
-                p.P0_00
-                    .into(),
-                p.P0_14
-                    .into(),
+static REMEMBER_BASSLINE: Song = Song::new(
+    &[Phrase::new(
+        &[
+            Note::new(
+                Some(NF::B2),
+                ND::Full,
             ),
-        )
-        .unwrap();
-}
-
-#[embassy_executor::task]
-async fn play_noise(
-    speaker_pin: Peri<'static, AnyPin>,
-    button_pin: Peri<'static, AnyPin>,
-) {
-    let mut speaker = Output::new(
-        speaker_pin,
-        Level::Low,
-        OutputDrive::Standard,
-    );
-    let mut button = Input::new(
-        button_pin,
-        Pull::Up,
-    );
-
-    loop {
-        LAMOUR
-            .play(&mut speaker)
-            .await;
-
-        // use Note as N;
-        // for held_note in [N::B3, N::A3, N::G3, N::D3] {
-        //     N::Fs4
-        //         .play(
-        //             &mut speaker,
-        //             TONE_FULL,
-        //         )
-        //         .await;
-        //     Timer::after_millis(MS_BETWEEN_TONES);
-
-        //     N::Fs4
-        //         .play(
-        //             &mut speaker,
-        //             TONE_FULL,
-        //         )
-        //         .await;
-        //     Timer::after_millis(MS_BETWEEN_TONES);
-
-        //     N::Fs4
-        //         .play(
-        //             &mut speaker,
-        //             TONE_HALF,
-        //         )
-        //         .await;
-        //     Timer::after_millis(MS_BETWEEN_TONES);
-
-        //     N::G4
-        //         .play(
-        //             &mut speaker,
-        //             TONE_HALF,
-        //         )
-        //         .await;
-        //     Timer::after_millis(MS_BETWEEN_TONES);
-
-        //     N::Fs4
-        //         .play(
-        //             &mut speaker,
-        //             TONE_FULL,
-        //         )
-        //         .await;
-        //     Timer::after_millis(MS_BETWEEN_TONES);
-
-        //     N::D4
-        //         .play(
-        //             &mut speaker,
-        //             TONE_FULL,
-        //         )
-        //         .await;
-        //     Timer::after_millis(MS_BETWEEN_TONES);
-
-        //     held_note
-        //         .play(
-        //             &mut speaker,
-        //             TONE_FULL * 2,
-        //         )
-        //         .await;
-
-        //     Timer::after_millis(MS_BETWEEN_PHRASES);
-        // }
-
-        button
-            .wait_for_falling_edge()
-            .await;
-    }
-}
+            Note::new(
+                Some(NF::D3),
+                ND::Full,
+            ),
+            Note::new(
+                Some(NF::G2),
+                ND::Full,
+            ),
+            Note::new(
+                Some(NF::G2),
+                ND::Full,
+            ),
+            Note::new(
+                Some(NF::Fs2),
+                ND::Full,
+            ),
+            Note::new(
+                Some(NF::D3),
+                ND::Full,
+            ),
+            Note::new(
+                Some(NF::G2),
+                ND::Full,
+            ),
+            Note::new(
+                Some(NF::G2),
+                ND::Full,
+            ),
+        ],
+        25,
+    )],
+    400,
+);
