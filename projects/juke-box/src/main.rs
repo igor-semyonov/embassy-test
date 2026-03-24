b0807788e7df5c316e843de9735ea0a882d0d4ae
Igor Semyonov <igor@semyonov.xyz>
2026-03-24 00:26:46 -0400

    refactored and it is working, next going to add more songs

--- projects/juke-box/src/main.rs
+++ projects/juke-box/src/main.rs
@@ -121,143 +121,199 @@ impl NoteDuration {
     }
 }
 
-struct Song<const N: usize> {
-    notes: [Note; N],
+struct Phrase<const N_Notes: usize> {
+    notes: [Note; N_Notes],
+    pause: u64,
 }
-impl<const N: usize> Song<N> {
+impl<const N_Notes: usize> Phrase<N_Notes> {
+    const fn new(
+        notes: [Note; N_Notes],
+        pause: u64,
+    ) -> Self {
+        Self {
+            notes,
+            pause,
+        }
+    }
     async fn play(&self, speaker: &mut Output<'_>) {
         for note in &self.notes {
-            note.play(speaker).await;
-            Timer::after_millis(MS_BETWEEN_TONES).await;
+            note.play(speaker)
+                .await;
+            Timer::after_millis(self.pause).await;
+        }
+    }
+}
+
+struct Song<
+    const N_Phrases: usize,
+    const N_Notes_per_Phrase: usize,
+> {
+    phrases: [Phrase<N_Notes_per_Phrase>; N_Phrases],
+    pause: u64,
+}
+impl<
+    const N_Phrases: usize,
+    const N_Notes_per_Phrase: usize,
+> Song<N_Phrases, N_Notes_per_Phrase>
+{
+    const fn new(
+        phrases: [Phrase<N_Notes_per_Phrase>; N_Phrases],
+        pause: u64,
+    ) -> Self {
+        Self {
+            phrases,
+            pause,
+        }
+    }
+    async fn play(&self, speaker: &mut Output<'_>) {
+        for phrase in &self.phrases {
+            phrase
+                .play(speaker)
+                .await;
+            Timer::after_millis(self.pause).await;
         }
     }
 }
 
 use NoteDuration as ND;
 use NoteFrequency as NF;
-static LAMOUR: Song<28> = Song {
-    notes: [
-        // phrase 1
-        Note::new(
-            NF::Fs4,
-            ND::Full,
+static LAMOUR: Song<4, 7> = Song::new(
+    [
+        Phrase::new(
+            [
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::G4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::D4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::B3,
+                    ND::Full,
+                ),
+            ],
+            25,
         ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
+        Phrase::new(
+            [
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::G4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::D4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::A3,
+                    ND::Full,
+                ),
+            ],
+            25,
         ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
+        Phrase::new(
+            [
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::G4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::D4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::G3,
+                    ND::Full,
+                ),
+            ],
+            25,
         ),
-        Note::new(
-            NF::Fs4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::G4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::D4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::B3,
-            ND::Full,
-        ),
-
-        // phrase 2
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::G4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::D4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::A3,
-            ND::Full,
-        ),
-
-        // phrase 3
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::G4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::D4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::G3,
-            ND::Full,
-        ),
-
-        // phrase 4
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::Fs4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::G4,
-            ND::Half,
-        ),
-        Note::new(
-            NF::D4,
-            ND::Full,
-        ),
-        Note::new(
-            NF::D3,
-            ND::Full,
+        Phrase::new(
+            [
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::Fs4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::G4,
+                    ND::Half,
+                ),
+                Note::new(
+                    NF::D4,
+                    ND::Full,
+                ),
+                Note::new(
+                    NF::D3,
+                    ND::Full,
+                ),
+            ],
+            25,
         ),
     ],
-};
+    400,
+);
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -291,7 +347,9 @@ async fn play_noise(
     );
 
     loop {
-        LAMOUR.play(&mut speaker).await;
+        LAMOUR
+            .play(&mut speaker)
+            .await;
 
         // use Note as N;
         // for held_note in [N::B3, N::A3, N::G3, N::D3] {
